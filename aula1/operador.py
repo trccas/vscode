72b2060bdff05cde5fc602e786e5c4c22bf5ee8a
trccas <168116598+trccas@users.noreply.github.com>
2025-09-07 19:22:59 -0300

    Add files via upload

---
+++ aula1/operador.py
@@ -0,0 +1,32 @@
+nome = "Claudio Aparecido"
+nome = "Tiago Henrique"
+nome = "Bruno Henrique"
+nome = "Ricardo Henrique"
+
+#Qual nome ele vai escrever?
+
+print(nome)
+
+idade = 47
+cidade = "Cotia"
+escola = "Senai"
+
+print (nome, idade, cidade, escola)
+
+#Operadores matematicos
+
+resultado = idade + 20
+# Qual valor fica no resultado
+print(resultado)
+
+#Subtração
+resultado2 = idade -20
+print(resultado2)
+
+#Multiplicação
+resultado3 = idade -20
+print(resultado3)
+
+#Divisão
+resultado4= idade / 2
+print(resultado4)
